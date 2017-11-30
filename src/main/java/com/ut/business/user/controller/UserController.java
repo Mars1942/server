673558ddfbaaac1d2716cb49bb6b23ddf199700a
Mars1942 <623464346@qq.com>
2017-11-30 21:35:41 +0800

    修改登录接口改用@requestBody接收

--- src/main/java/com/ut/business/user/controller/UserController.java
+++ src/main/java/com/ut/business/user/controller/UserController.java
@@ -5,10 +5,7 @@ import com.ut.business.user.domain.User;
 import com.ut.business.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,17 +16,16 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
     public Page<User> firstPage(@RequestParam("pageNumber") int pageNumber){
 
         return userService.findAll(pageNumber, Constant.PAGE_SIZE);
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public User login(@RequestParam("name") String name, @RequestParam("passWord") String passWord, HttpServletRequest request) {
-        String a = request.getParameter("name");
-        System.out.print(a);
-        return userService.findByLoginNameAndPassWord(name,passWord);
+    public User login(@RequestBody User user) {
+
+        return userService.findByLoginNameAndPassWord(user.getName(),user.getPassWord());
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
